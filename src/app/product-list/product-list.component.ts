a9412393fc8a23ddde5a9656a84d1e3958b6da84
Benedikt Schätz <benedikt.schaetz@grs.de>
2022-05-25 08:47:46 +0200

    porductlist add share button

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,7 @@ export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert('The product has been shared!');
+    console.log('The product has been shared!');
   }
 }
 
